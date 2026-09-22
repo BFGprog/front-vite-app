4c06049fa8e7ab8d20a5ef587327a8de68e2254c
BFGprog <karpov0005@gmail.com>
2026-09-22 21:43:31 +0300

    add import in mainPage

--- src/pages/cable/CableMainPage.jsx
+++ src/pages/cable/CableMainPage.jsx
@@ -1,12 +1,13 @@
 import CableUploadPage from "../../cableElement/CableUploadPage";
 import CableReportsPage from "../../cableElement/CableReportsPage";
+import CableDocumentPage from "../../cableElement/CableDocumentPage";
 
 export default function CableMainPage() {
   return (
     <div>
       <CableUploadPage />
 
-      
+      <CableDocumentPage />
 
       <CableReportsPage />
     </div>
